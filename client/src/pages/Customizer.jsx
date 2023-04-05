1f1286267fab05825955ff85ac25376a222a864d
dutdavid <dutdavid7@gmail.com>
2023-04-05 22:16:05 +0000

    setup

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Customizer = () => {
   return (
-    <div>Customizer</div>
+    <button className='download-btn' onClick={downloadCanvasToImage}>
+    <img
+      src={download}
+      alt='download_image'
+      className='w-3/5 h-3/5 object-contain'
+    />
+  </button>
   )
 }
 
